32d0a1d47fd2f2dbfbb6fc6428fcea1c406ce844
odootest2023 <odootest2023@163.com>
2026-08-16 20:34:39 +0800

    chore: SheetGrid export（components 入口）

--- src/components/index.ts
+++ src/components/index.ts
@@ -161,6 +161,8 @@ export { Markdown } from './Markdown/Markdown.ts'
 export type { MarkdownProps } from './Markdown/Markdown.ts'
 export { parseMarkdown, parseInline } from './Markdown/parser.ts'
 export { FilePreview } from './FilePreview/FilePreview.ts'
+export { SheetGrid } from './SheetGrid/SheetGrid.ts'
+export type { SheetGridProps } from './SheetGrid/SheetGrid.ts'
 export type { FilePreviewProps, FileType } from './FilePreview/FilePreview.ts'
 export { markdownToHtml, serializeMarkdown } from './FilePreview/markdown.ts'
 
